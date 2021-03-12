e5cf2807be80a5d1b6404ca5c4df5a7443fe1f22
PJColombo <paulo.colombo@pm.me>
2021-03-12 15:08:20 +0100

    Hatch general flow test when max goal is reached implemented

--- packages/hardhat/test/hatch.ts
+++ packages/hardhat/test/hatch.ts
@@ -2,87 +2,118 @@ import { Signer } from "@ethersproject/abstract-signer";
 import { ethers } from "hardhat";
 import { use, expect } from "chai";
 import { solidity } from "ethereum-waffle";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-import newHatch, { COLLATERAL_TOKEN, SCORE_TOKEN } from "../scripts/new-hatch";
+import newHatch from "../scripts/new-hatch";
 import { impersonateAddress } from "../helpers/rpc";
-import { Kernel, IHatch, IImpactHours, ERC20 } from "../typechain";
+import { Kernel, IHatch, IImpactHours, ERC20, MiniMeToken } from "../typechain";
 import { BigNumber } from "@ethersproject/bignumber";
+import { Console, log } from "console";
 
 use(solidity);
 
-const CONTRIBUTORS_PROCESSED_PER_TRANSACTION = 10
+// There are multiple ERC20 paths. We need to specify one.
+const ERC20Path = "@aragon/os/contracts/lib/token/ERC20.sol:ERC20";
+const HATCH_USER = "0xDc2aDfA800a1ffA16078Ef8C1F251D50DcDa1065";
+const CONTRIBUTORS_PROCESSED_PER_TRANSACTION = 10;
+const TOKEN_DECIMALS = 1e18;
 
-async function getContributors() {
-  return fetch(
-    'https://api.thegraph.com/subgraphs/name/aragon/aragon-tokens-xdai',
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        query: `
+async function getContributors(tokenAddress) {
+  return fetch("https://api.thegraph.com/subgraphs/name/aragon/aragon-tokens-xdai", {
+    method: "POST",
+    body: JSON.stringify({
+      query: `
           {
-            tokenHolders(first: 1000 where : { tokenAddress: "${SCORE_TOKEN}"}) {
+            tokenHolders(first: 1000 where : { tokenAddress: "${tokenAddress}"}) {
               address
             }
           }
-        `})
-    }
-  )
-  .then(res => res.json())
-  .then(res => res.data.tokenHolders.map(({ address }) => address))
+        `,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => res.data.tokenHolders.map(({ address }) => address));
 }
 
-const HATCH_USER = "0xDc2aDfA800a1ffA16078Ef8C1F251D50DcDa1065";
-
 describe.only("Hatch Template", function () {
-  let signers: Signer[];
   let hatchUser: Signer;
   let dao: Kernel;
-  let daoAddress: string;
-  let hatchAddress: string;
+  let hatch: IHatch;
+  let impactHours: IImpactHours;
+  let contributionToken: ERC20;
+  let ihTokenAddress: string;
 
   before(async () => {
-    [daoAddress, hatchAddress] = await newHatch();
-    dao = (await ethers.getContractAt("Kernel", daoAddress)) as Kernel;
+    const [daoAddress, hatchAddress, impactHoursAddress] = await newHatch();
+
+    console.log("DAO created using Hatch Template");
+
     hatchUser = await impersonateAddress(HATCH_USER);
-    signers = await ethers.getSigners();
+    dao = (await ethers.getContractAt("Kernel", daoAddress)) as Kernel;
+    hatch = (await ethers.getContractAt("IHatch", hatchAddress, hatchUser)) as IHatch;
+    impactHours = (await ethers.getContractAt("IImpactHours", impactHoursAddress, hatchUser)) as IImpactHours;
+    const contributionTokenAddress = await hatch.contributionToken();
+    contributionToken = (await ethers.getContractAt(ERC20Path, contributionTokenAddress, hatchUser)) as ERC20;
+    const ihClonedTokenAddress = await impactHours.token();
+    const ihClonedToken = (await ethers.getContractAt("MiniMeToken", ihClonedTokenAddress, hatchUser)) as MiniMeToken;
+    ihTokenAddress = await ihClonedToken.parentToken();
   });
 
-  describe("Test Hatch Flow", async function () {
-    it("Get Hatch Address", async function () {
+  describe("Test General Flow", async function () {
+    it("Test Hatch Flow when max goal is reached", async function () {
+      let tx,
+        txReceipt,
+        totalGasUsed = BigNumber.from("0");
 
-      const hatch = (await ethers.getContractAt("IHatch", hatchAddress, hatchUser)) as IHatch;
+      tx = await hatch.open();
+      txReceipt = await tx.wait();
 
-      await hatch.open();
+      console.log(`Hatch Opened. Gas used: ${txReceipt.gasUsed}`);
+      totalGasUsed = totalGasUsed.add(txReceipt.gasUsed);
 
-      const token = (await ethers.getContractAt(
-        "@aragon/os/contracts/lib/token/ERC20.sol:ERC20",
-        COLLATERAL_TOKEN,
-        hatchUser
-      )) as ERC20;
+      const contributionAmount = BigNumber.from("1000").mul(String(TOKEN_DECIMALS));
 
-      const value = BigNumber.from("1000").mul(String(1e18));
+      tx = await contributionToken.approve(hatch.address, contributionAmount);
+      txReceipt = await tx.wait();
 
-      await token.approve(hatch.address, value);
+      console.log(`Contribution amount approved. Gas used: ${txReceipt.gasUsed}`);
+      totalGasUsed = totalGasUsed.add(txReceipt.gasUsed);
 
-      await hatch.contribute(value, { gasLimit: 2000000 });
+      tx = await hatch.contribute(contributionAmount);
+      txReceipt = await tx.wait();
 
-      const impactHours = (await ethers.getContractAt("IImpactHours", hatchAddress, hatchUser)) as IImpactHours;
+      console.log(`Contribution amount ${contributionAmount} made by ${HATCH_USER}. Gas used: ${txReceipt.gasUsed}`);
+      totalGasUsed = totalGasUsed.add(txReceipt.gasUsed);
 
-      const contributors = await getContributors()
+      const contributors = await getContributors(ihTokenAddress);
+
+      console.log("IH token holders fetched");
+
       const total = Math.ceil(contributors.length / CONTRIBUTORS_PROCESSED_PER_TRANSACTION);
       let counter = 1;
+
       for (let i = 0; i < contributors.length; i += CONTRIBUTORS_PROCESSED_PER_TRANSACTION) {
-        const tx = await impactHours.claimReward(
-          contributors.slice(i, i + CONTRIBUTORS_PROCESSED_PER_TRANSACTION),
-          { gasLimit: 2000000 }
+        tx = await impactHours.claimReward(contributors.slice(i, i + CONTRIBUTORS_PROCESSED_PER_TRANSACTION), {
+          gasPrice: 1,
+        });
+        txReceipt = await tx.wait();
+
+        console.log(
+          `Rewards claimed for IH token holders ${i + 1} to ${Math.min(
+            i + CONTRIBUTORS_PROCESSED_PER_TRANSACTION,
+            contributors.length
+          )}. Tx ${counter++} of ${total}. Gas fee: ${txReceipt.gasUsed}`
         );
-        const txReceipt = await tx.wait();
-        console.log(`Impact hours Txs: ${counter++} of ${total}. Claimed ${i + 1} to ${Math.min(i + CONTRIBUTORS_PROCESSED_PER_TRANSACTION, contributors.length)} impact hours. Gas fee: ${txReceipt.gasUsed}`)
+        totalGasUsed = totalGasUsed.add(txReceipt.gasUsed);
       }
-      const txClose = await hatch.close({ gasLimit: 2000000 });
-      const txCloseReceipt = await txClose.wait();
-      console.log(`Hatch closed successfully. Gas used: ${txCloseReceipt.gasUsed}`)
+
+      tx = await hatch.close();
+      txReceipt = await tx.wait();
+
+      console.log(`Hatch closed. Gas used: ${txReceipt.gasUsed}`);
+      totalGasUsed = totalGasUsed.add(txReceipt.gasUsed);
+
+      console.log(`Total gas used: ${totalGasUsed.toString()}`);
     });
   });
 });
