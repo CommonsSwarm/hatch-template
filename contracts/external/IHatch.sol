30e5acc938a18475ebdd48e971dbd90f73636d34
PJColombo <paulo.colombo@pm.me>
2021-03-16 01:36:54 +0100

    External contract interfaces updated

--- contracts/external/IHatch.sol
+++ contracts/external/IHatch.sol
@@ -1,10 +1,14 @@
 pragma solidity ^0.4.24;
 
+import "@aragon/os/contracts/lib/token/ERC20.sol";
+
 contract IHatch {
 
     bytes32 public constant OPEN_ROLE = keccak256("OPEN_ROLE");
     bytes32 public constant CONTRIBUTE_ROLE = keccak256("CONTRIBUTE_ROLE");
     bytes32 public constant CLOSE_ROLE = keccak256("CLOSE_ROLE");
+
+    ERC20                                           public token;
 
     address                                         public reserve;
     address                                         public beneficiary;
@@ -47,5 +51,6 @@ contract IHatch {
 
     function close() external;
     function state() external view returns (uint8);
+    function contributionToTokens(uint256 _value) public view returns (uint256);
 
 }
