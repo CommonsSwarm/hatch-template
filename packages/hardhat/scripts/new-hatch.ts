82c0df20c18eb53fc386b8556c405320fa4a1257
PJColombo <paulo.colombo@pm.me>
2021-03-16 23:40:55 +0100

    Hatch Template constants refactored. Helpers refactored

--- packages/hardhat/scripts/new-hatch.ts
+++ packages/hardhat/scripts/new-hatch.ts
@@ -3,21 +3,22 @@ import { Contract } from "@ethersproject/contracts";
 import { Signer } from "@ethersproject/abstract-signer";
 import { ERC20, HatchTemplate, IHatch, IImpactHours, Kernel, MiniMeToken } from "../typechain";
 import { impersonateAddress } from "../helpers/rpc";
+import getParams from "../params";
 
 const { deployments } = hre;
-const { BigNumber } = ethers;
 
 export interface HatchContext {
   hatchUser?: Signer;
   dao?: Kernel;
   hatch?: IHatch;
   contributionToken?: ERC20;
-  hatchToken: ERC20;
-  impactHours: IImpactHours;
-  impactHoursClonedToken: MiniMeToken;
-  impactHoursToken: MiniMeToken;
+  hatchToken?: ERC20;
+  impactHours?: IImpactHours;
+  impactHoursClonedToken?: MiniMeToken;
+  impactHoursToken?: MiniMeToken;
 }
 
+// Script arguments
 const DAO_ID = "testtec" + Math.random(); // Note this must be unique for each deployment, change it for subsequent deployments
 const NETWORK_ARG = "--network";
 const DAO_ID_ARG = "--daoid";
@@ -28,85 +29,38 @@ const argValue = (arg, defaultValue) =>
 const network = () => argValue(NETWORK_ARG, "local");
 const daoId = () => argValue(DAO_ID_ARG, DAO_ID);
 
-// Helpers, no need to change
-const HOURS = 60 * 60;
-const DAYS = 24 * HOURS;
-const ONE_HUNDRED_PERCENT = 1e18;
-const ONE_TOKEN = BigNumber.from((1e18).toString());
-const FUNDRAISING_ONE_HUNDRED_PERCENT = 1e6;
-const FUNDRAISING_ONE_TOKEN = BigNumber.from((1e18).toString());
-const PPM = 1000000;
-
 const BLOCKTIME = network() === "rinkeby" ? 15 : network() === "mainnet" ? 13 : 5; // 15 rinkeby, 13 mainnet, 5 xdai
+
 console.log(`Every ${BLOCKTIME}s a new block is mined in ${network()}.`);
 
-// CONFIGURATION
-
-// Collateral Token is used to pay contributors and held in the bonding curve reserve
-const COLLATERAL_TOKEN = "0xfb8f60246d56905866e12443ec0836ebfb3e1f2e"; // tDAI
-
-// Org Token represents membership in the community and influence in proposals
-const ORG_TOKEN_NAME = "Token Engineering Commons TEST Hatch Token";
-const ORG_TOKEN_SYMBOL = "TESTTECH";
-
-// # Hatch Oracle Settings
-
-// Score membership token is used to check how much members can contribute to the hatch
-const SCORE_TOKEN = "0xc4fbe68522ba81a28879763c3ee33e08b13c499e"; // CSTK Token on xDai
-const SCORE_ONE_TOKEN = BigNumber.from(1);
-// Ratio contribution tokens allowed per score membership token
-const HATCH_ORACLE_RATIO = BigNumber.from(0.8 * PPM)
-  .mul(FUNDRAISING_ONE_TOKEN)
-  .div(SCORE_ONE_TOKEN);
-
-// # Dandelion Voting Settings
-
-// Used for administrative or binary choice decisions with ragequit-like functionality
-const SUPPORT_REQUIRED = String(0.6 * ONE_HUNDRED_PERCENT);
-const MIN_ACCEPTANCE_QUORUM = String(0.02 * ONE_HUNDRED_PERCENT);
-const VOTE_DURATION_BLOCKS = (3 * DAYS) / BLOCKTIME;
-const VOTE_BUFFER_BLOCKS = (8 * HOURS) / BLOCKTIME;
-const VOTE_EXECUTION_DELAY_BLOCKS = (24 * HOURS) / BLOCKTIME;
-// Set the fee paid to the org to create an administrative vote
-const TOLLGATE_FEE = BigNumber.from(3).mul(ONE_TOKEN);
-
-// # Hatch settings
-
-// How many COLLATERAL_TOKEN's are required to Hatch
-const HATCH_MIN_GOAL = BigNumber.from(5).mul(ONE_TOKEN);
-// What is the Max number of COLLATERAL_TOKEN's the Hatch can recieve
-const HATCH_MAX_GOAL = BigNumber.from(1000).mul(ONE_TOKEN);
-// How long should the hatch period last
-const HATCH_PERIOD = 15 * DAYS;
-// How many organization tokens should be minted per collateral token
-const HATCH_EXCHANGE_RATE = BigNumber.from(10000 * PPM)
-  .mul(ONE_TOKEN)
-  .div(FUNDRAISING_ONE_TOKEN);
-// When does the cliff for vesting restrictions end
-const VESTING_CLIFF_PERIOD = HATCH_PERIOD + 1; // 1 second after hatch
-// When will the Hatchers be fully vested and able to use the redemptions app
-const VESTING_COMPLETE_PERIOD = VESTING_CLIFF_PERIOD + 1; // 2 seconds after hatch
-// What percentage of Hatch contributions should go to the Funding Pool and therefore be non refundable
-const HATCH_TRIBUTE = 0.05 * FUNDRAISING_ONE_HUNDRED_PERCENT;
-// when should the Hatch open, setting 0 will allow anyone to open the Hatch anytime after deployment
-const OPEN_DATE = 0;
-
-// # Impact hours settings
-
-// Impact Hours token address
-const IH_TOKEN = "0xdf2c3c8764a92eb43d2eea0a4c2d77c2306b0835";
-// Max theoretical collateral token rate per impact hour
-const MAX_IH_RATE = BigNumber.from(100).mul(ONE_TOKEN);
-
-// How much will we need to raise to reach 1/2 of the MAX_IH_RATE divided by total IH
-const EXPECTED_RAISE_PER_IH = BigNumber.from(0.012 * 1000)
-  .mul(ONE_TOKEN)
-  .div(1000);
+const {
+  ORG_TOKEN_NAME,
+  ORG_TOKEN_SYMBOL,
+  SUPPORT_REQUIRED,
+  MIN_ACCEPTANCE_QUORUM,
+  VOTE_DURATION_BLOCKS,
+  VOTE_BUFFER_BLOCKS,
+  VOTE_EXECUTION_DELAY_BLOCKS,
+  COLLATERAL_TOKEN,
+  IH_TOKEN,
+  EXPECTED_RAISE_PER_IH,
+  ONE_TOKEN,
+  HATCH_MIN_GOAL,
+  HATCH_MAX_GOAL,
+  HATCH_PERIOD,
+  HATCH_EXCHANGE_RATE,
+  VESTING_CLIFF_PERIOD,
+  VESTING_COMPLETE_PERIOD,
+  HATCH_TRIBUTE,
+  OPEN_DATE,
+  MAX_IH_RATE,
+  TOLLGATE_FEE,
+  SCORE_TOKEN,
+  HATCH_ORACLE_RATIO,
+} = getParams(BLOCKTIME);
 
 // There are multiple ERC20 paths. We need to specify one.
 const ERC20Path = "@aragon/os/contracts/lib/token/ERC20.sol:ERC20";
-// Address use to perform hatch operations
-const HATCH_USER = "0xDc2aDfA800a1ffA16078Ef8C1F251D50DcDa1065";
 
 const hatchTemplateAddress = async () => (await deployments.get("HatchTemplate")).address;
 
@@ -165,7 +119,7 @@ const createDaoTxOne = async (context: HatchContext, appManager: Signer, log: Fu
 
 const createDaoTxTwo = async (context: HatchContext, appManager: Signer, log: Function): Promise<void> => {
   const hatchTemplate = await getHatchTemplate(appManager);
-  const hatchUser = context.hatchUser;
+  const hatchUser = context.hatchUser ? context.hatchUser : appManager;
   const impactHoursToken = (await ethers.getContractAt("MiniMeToken", IH_TOKEN, appManager)) as MiniMeToken;
 
   const totalImpactHours = await impactHoursToken.totalSupply();
@@ -218,7 +172,7 @@ const createDaoTxThree = async (context: HatchContext, appManager: Signer, log: 
   const hatchTemplate = await getHatchTemplate(appManager);
 
   const tx = await hatchTemplate.createDaoTxThree(
-    DAO_ID,
+    daoId(),
     [COLLATERAL_TOKEN],
     COLLATERAL_TOKEN,
     TOLLGATE_FEE,
@@ -231,11 +185,13 @@ const createDaoTxThree = async (context: HatchContext, appManager: Signer, log: 
   log(`Tx three completed: Tollgate, Redemptions and Conviction Voting apps set up.`);
 };
 
-export default async function main(log = console.log) {
+export default async function main(hatchUserAddress?: string, log = console.log) {
   const hatchTemplateContext = {} as HatchContext;
   const appManager = await ethers.getSigners()[0];
 
-  hatchTemplateContext.hatchUser = await impersonateAddress(HATCH_USER);
+  if (hatchUserAddress) {
+    hatchTemplateContext.hatchUser = await impersonateAddress(hatchUserAddress);
+  }
 
   await createDaoTxOne(hatchTemplateContext, appManager, log);
   await createDaoTxTwo(hatchTemplateContext, appManager, log);
