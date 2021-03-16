1a4d9a1ba5a87b73cf41f23811d2ff24818947b8
PJColombo <paulo.colombo@pm.me>
2021-03-16 01:36:54 +0100

    External contract interfaces updated

--- packages/hardhat/contracts/external/IImpactHours.sol
+++ packages/hardhat/contracts/external/IImpactHours.sol
@@ -8,9 +8,12 @@ contract IImpactHours {
     using SafeERC20 for ERC20;
 
     ERC20 public token;
+    uint256 public maxRate;
+    uint256 public expectedRaise;
 
-    bytes32 public constant CLAIM_ROLE = keccak256("CLAIM_ROLE");
-    function initialize(address _token, address _hatch, uint256 _maxRate, uint256 _expectedRaisePerIH) external;
+    bytes32 public constant CLOSE_ROLE = keccak256("CLOSE_ROLE");
+
+    function initialize(address _token, address _hatch, uint256 _maxRate, uint256 _expectedRaise) external;
     function claimReward(address[] _contributors) external;
-    function canPerform(address, address, bytes32, uint256[]) external view;
+    function closeHatch() external;
 }
