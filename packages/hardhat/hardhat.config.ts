7b07bb49abee15958ce55833cc534187a78df7f5
PJColombo <paulo.colombo@pm.me>
2021-03-20 13:30:20 +0100

    block number updated

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -95,7 +95,7 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: true,
       forking: {
         url: "https://xdai-archive.blockscout.com",
-        blockNumber: 15032477,
+        blockNumber: 15094013,
       },
     },
     localhost: {
