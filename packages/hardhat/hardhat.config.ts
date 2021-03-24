0e3ddc1da993c92864658a9e07b0465ad09ef88a
David Llop <931684+sembrestels@users.noreply.github.com>
2021-03-24 15:50:42 +0100

    Money can be refunded by any account

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -95,7 +95,7 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: true,
       forking: {
         url: "https://xdai-archive.blockscout.com",
-        blockNumber: 15166265,
+        blockNumber: 15177332,
       },
     },
     localhost: {
